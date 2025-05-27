486aedb84eeb965ae1de1212aa051c1e81afa1ea
Gab <gabjoankam@gmail.com>
2025-05-27 21:45:40 +0300

    class utils.h

--- utils.h
+++ utils.h
@@ -1,26 +1,26 @@
-
 #ifndef UTILS_H
 #define UTILS_H
 
 #include "Lib.h"
-#include "student.h"
 
 enum ContainerType { VECTOR, LIST, DEQUE };
 
 // Nuskaitymo funkcija:
 template <typename Container>
-void skaityti(const string& filename, Container& students) {
+void skaityti(const std::string& filename, Container& students) {
     ifstream fin(filename);
-    Studentas s;
     string header;
     getline(fin, header);
-    while (fin >> s.vardas >> s.pavarde) {
-        s.nd.clear();
+    string vardas, pavarde;
+    while (fin >> vardas >> pavarde) {
+        vector<int> nd;
         int n;
         for (int i = 0; i < 5; i++) {
-            if (fin >> n) s.nd.push_back(n);
+            if (fin >> n) nd.push_back(n);
         }
-        fin >> s.egzaminas;
+        int egzaminas;
+        fin >> egzaminas;
+        Studentas s(vardas, pavarde, nd, egzaminas);
         s.skaiciuoti_galutini();
         students.push_back(s);
     }
@@ -30,18 +30,18 @@ void skaityti(const string& filename, Container& students) {
 template <typename Container>
 void rusiavimas(Container& students) {
     sort(students.begin(), students.end(),
-        [](const Studentas& a, const Studentas& b) { return a.galutinis < b.galutinis; });
+        [](const Studentas& a, const Studentas& b) { return a.galutinis() < b.galutinis(); });
 }
 template <>
 inline void rusiavimas(list<Studentas>& students) {
-    students.sort([](const Studentas& a, const Studentas& b) { return a.galutinis < b.galutinis; });
+    students.sort([](const Studentas& a, const Studentas& b) { return a.galutinis() < b.galutinis(); });
 }
 
 // 1 strategija: Kopijuojama į du naujus konteinerius
 template <typename Container>
 void skirstymas1(const Container& students, Container& vargs, Container& kiet) {
     for (const auto& s : students) {
-        if (s.galutinis < 5.0) vargs.push_back(s);
+        if (s.galutinis() < 5.0) vargs.push_back(s);
         else kiet.push_back(s);
     }
 }
@@ -51,7 +51,7 @@ template <typename Container>
 void skirstymas2(Container& students, Container& vargs) {
     auto it = students.begin();
     while (it != students.end()) {
-        if (it->galutinis < 5.0) {
+        if (it->galutinis() < 5.0) {
             vargs.push_back(*it);
             it = students.erase(it);
         } else ++it;
@@ -62,7 +62,7 @@ void skirstymas2(Container& students, Container& vargs) {
 template <typename Container>
 void skirstymas3(Container& students, Container& vargs) {
     auto it = partition(students.begin(), students.end(), [](const Studentas& s) {
-        return s.galutinis < 5.0;
+        return s.galutinis() < 5.0;
     });
     vargs.insert(vargs.end(), students.begin(), it);
     students.erase(students.begin(), it);
@@ -70,7 +70,7 @@ void skirstymas3(Container& students, Container& vargs) {
 template <>
 inline void skirstymas3(list<Studentas>& students, list<Studentas>& vargs) {
     students.remove_if([&vargs](const Studentas& s) {
-        if (s.galutinis < 5.0) {
+        if (s.galutinis() < 5.0) {
             vargs.push_back(s);
             return true;
         }
