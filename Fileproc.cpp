5d9aec07485926ead859c5a1eaab2012d920e593
Gab <gabjoankam@gmail.com>
2025-05-27 21:41:14 +0300

    Class Fileproc.cpp

--- Fileproc.cpp
+++ Fileproc.cpp
@@ -1,4 +1,5 @@
 #include "Fileproc.h"
+#include "student.h"
 
 void nuskaitytiFaila(const string& pav, vector<Studentas>& studentai) {
     ifstream fin(pav);
@@ -7,13 +8,14 @@ void nuskaitytiFaila(const string& pav, vector<Studentas>& studentai) {
 
     while (getline(fin, eilute)) {
         istringstream iss(eilute);
-        Studentas s;
-        iss >> s.vardas >> s.pavarde;
+        string vardas, pavarde;
+        vector<int> nd;
         int paz;
-        while (iss >> paz) s.namuDarbai.push_back(paz);
-        s.egzaminas = s.namuDarbai.back(); s.namuDarbai.pop_back();
-
-        s.galutinisVid = 0.4 * (accumulate(s.namuDarbai.begin(), s.namuDarbai.end(), 0.0) / s.namuDarbai.size()) + 0.6 * s.egzaminas;
+        iss >> vardas >> pavarde;
+        while (iss >> paz) nd.push_back(paz);
+        int egzaminas = nd.back(); nd.pop_back();
+        Studentas s(vardas, pavarde, nd, egzaminas);
+        s.skaiciuoti_galutini();
         studentai.push_back(s);
     }
     fin.close();
@@ -21,15 +23,15 @@ void nuskaitytiFaila(const string& pav, vector<Studentas>& studentai) {
 
 void padalintiStudentus(const vector<Studentas>& studentai, vector<Studentas>& vargsai, vector<Studentas>& galvociai) {
     for (const auto& s : studentai) {
-        if (s.galutinisVid < 5.0) vargsai.push_back(s);
+        if (s.galutinis() < 5.0) vargsai.push_back(s);
         else galvociai.push_back(s);
     }
 }
 
 void isvestiIFailus(const vector<Studentas>& vargsai, const vector<Studentas>& galvociai, const string& pavPre) {
     ofstream foutV(pavPre + "_vargsiukai.txt");
-    for (const auto& s : vargsai) foutV << s.vardas << " " << s.pavarde << " " << s.galutinisVid << "\n";
+    for (const auto& s : vargsai) foutV << s.vardas() << " " << s.pavarde() << " " << s.galutinis() << "\n";
 
     ofstream foutG(pavPre + "_galvociai.txt");
-    for (const auto& s : galvociai) foutG << s.vardas << " " << s.pavarde << " " << s.galutinisVid << "\n";
+    for (const auto& s : galvociai) foutG << s.vardas() << " " << s.pavarde() << " " << s.galutinis() << "\n";
 }
