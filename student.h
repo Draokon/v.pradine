c983ca4a53b3f6b57125641924f738ad41e1e015
Gab <gabjoankam@gmail.com>
2025-05-27 21:51:36 +0300

    student.h

---
+++ student.h
@@ -0,0 +1,27 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include "Lib.h"
+struct Studentas {
+    string vardas;
+    string pavarde;
+    vector<int> nd;
+    int egzaminas;
+    double galutinisVid;
+    double galutinisMed;
+    double galutinis;
+
+    void skaiciuoti_galutini() {
+        if (nd.empty()) {
+            galutinisVid = 0;
+            galutinis = 0.4 * 0 + 0.6 * egzaminas;
+        } else {
+            double vid = 0;
+            for (auto n : nd) vid += n;
+            vid /= nd.size();
+            galutinisVid = vid;
+            galutinis = 0.4 * vid + 0.6 * egzaminas;
+        }
+    }
+};
+#endif
