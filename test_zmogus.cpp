d7a18d81565233eb75584e44121740d8261f2496
Gab <gabjoankam@gmail.com>
2025-05-28 01:30:34 +0300

    test_zmogus.cpp

---
+++ test_zmogus.cpp
@@ -0,0 +1,20 @@
+//testas, kad zmogus abstraktus
+#include "zmogus.h"
+#include "student.h"
+#include <memory>
+#include <iostream>
+
+// Šis kodas NEGALI kompiliuotis: Zmogus z("a", "b"); // ERROR
+
+void abstrakcios_klases_testas() {
+    // std::unique_ptr<Zmogus> z = std::make_unique<Zmogus>("A", "B"); // ERROR
+    std::unique_ptr<Zmogus> s = std::make_unique<Studentas>("Vardas", "Pavarde", std::vector<int>{10, 9}, 8);
+    s->info(); // veikia, nes Studentas paveldi info()
+}
+int main() {
+    abstrakcios_klases_testas();
+    return 0;
+}
+
+//g++ test_zmogus.cpp zmogus.cpp student.cpp -o zmogus
+// zmogus.exe
